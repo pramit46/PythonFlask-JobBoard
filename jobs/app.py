542e07b69f0455ce1163ef8e350e5fe20f0293ab
pramit46 <pramit46@yahoo.com>
2018-12-29 22:19:18 +0530

    Update app.py

--- jobs/app.py
+++ jobs/app.py
@@ -30,5 +30,8 @@ def close_connection(exception):
 @app.route('/')
 @app.route('/jobs')
 def jobs():
-    jobs=execute_sql('SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id, employer.name as employer_name FROM job JOIN employer ON job.employer_id=employer.id')
-    return render_template('index.html', jobs=jobs)
+    #jobs = execute_sql('SELECT job.id, job.title, job.description, \
+    #job.salary, employer.id as employer_id, employer.name as employer_name \
+    #FROM job JOIN employer ON employer.id = job.employer_id')
+    #return render_template('index.html', jobs=jobs)
+    return render_template('index.html')
